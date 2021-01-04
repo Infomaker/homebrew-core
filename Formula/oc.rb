284428ccb604956db7a6398954418ffeb347951c
Tim Terlegård <tim.terlegard@infomaker.se>
2021-01-04 11:39:25 +0100

    make 0.0.3 available for linux as well

--- Formula/oc.rb
+++ Formula/oc.rb
@@ -1,8 +1,15 @@
 class Oc < Formula
   desc "Open Content CLI"
   homepage "https://bitbucket.org/infomaker/opencontent-cli/"
-  url "https://infomaker-public-artifacts.s3-eu-west-1.amazonaws.com/oc/darwin/oc-0.0.2.zip"
-  sha256 "0aa051d83fee2191e22b7b47fea9c97ac9d81bb8132c2d2c7df55946dcf55d3e"
+
+  if OS.mac?
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/oc/darwin/oc-0.0.3.zip"
+    sha256 "55fa51d395ae273dd5176ab987843945ab52047cfed9b1c2b2e7be842dbb80b6"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/oc/linux/oc-0.0.3.zip"
+    sha256 "4bd412a7f9a5a8f89e79758b8c22d9c3a899926906f652f3413f870dbccbfaf0"
+  end
 
   def install
     bin.install "oc"
