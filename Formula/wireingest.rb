b46a8214f9f5f204a47376369f9bcd5fdbe3a7d9
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-05 17:05:30 +0100

    Brew formula update for wireingest version v0.2.1

--- Formula/wireingest.rb
+++ Formula/wireingest.rb
@@ -2,16 +2,16 @@
 class Wireingest < Formula
   desc "Administrative tool for wire ingest config."
   homepage "https://bitbucket.org/infomaker/wireingest/"
-  version "0.2.0"
+  version "0.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/wireingest/0.2.0/wireingest_darwin_amd64.zip"
-    sha256 "9902cf9ac880999a7af69566f1ccf6fa3971f8a17359a856a833c11349e22e56"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/wireingest/0.2.1/wireingest_darwin_amd64.zip"
+    sha256 "77896564c35996506e1f2725cabe6ca15973911da9e4948bd30f800c9ebfff23"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/wireingest/0.2.0/wireingest_linux_amd64.zip"
-    sha256 "9445c536aedd3a23f75154461763bc278b6b86bfefe2ee443bef4016c37b35f0"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/wireingest/0.2.1/wireingest_linux_amd64.zip"
+    sha256 "90a9aaa71b7e6be7fe22593309a4e41a0523f5ab167a2a4bc5d3603c3888f484"
   end
 
   def install
