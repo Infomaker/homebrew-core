19daa062089d2969c0316ce92cae2995c7f4ffb5
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-23 16:36:31 +0200

    Brew formula update for deploy-manager version v0.1.2

--- Formula/deploy-manager.rb
+++ Formula/deploy-manager.rb
@@ -2,16 +2,16 @@
 class DeployManager < Formula
   desc "Utility that helps with deploy and setup of SaaS services"
   homepage "https://bitbucket.org/infomaker/deploy-manager/"
-  version "0.1.1"
+  version "0.1.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/deploy-manager/0.1.1/deploy-manager_darwin_amd64.zip"
-    sha256 "e544eeceab2fbc5d2931a25eca1290c4b8634caa7c064c12278d46f5eb13d03b"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/deploy-manager/0.1.2/deploy-manager_darwin_amd64.zip"
+    sha256 "c59dfc117af93925abaa993e3454ee9097a0d9b856215050033a75f70cba24ef"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/deploy-manager/0.1.1/deploy-manager_linux_amd64.zip"
-    sha256 "530719299e645fa421f7b9e2b8de26146a2011b7da1db84c5fcca430faf8ceea"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/deploy-manager/0.1.2/deploy-manager_linux_amd64.zip"
+    sha256 "a8c84bf2b6ff4ef6920b2b5c0607050582649896cf8f07530c869e743306f3a0"
   end
 
   def install
