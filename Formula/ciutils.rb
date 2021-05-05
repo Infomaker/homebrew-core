53c56b6f2ebade46e602237782196fae93b8fb9d
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-05 15:48:23 +0200

    Brew formula update for ciutils version v0.6.1

--- Formula/ciutils.rb
+++ Formula/ciutils.rb
@@ -2,16 +2,16 @@
 class Ciutils < Formula
   desc "Common CI workflows"
   homepage "https://bitbucket.org/infomaker/ciutils/"
-  version "0.5.1"
+  version "0.6.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/ciutils/0.5.1/ciutils_darwin_amd64.tar.gz"
-    sha256 "9dab7390ee6cf37278190e64e3bfd1e7528fe156e4da4dbdc999583b0c117fa2"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/ciutils/0.6.1/ciutils_darwin_amd64.tar.gz"
+    sha256 "a131eefa173ca0257e6f87d4fb5ab09eec9e3dbf720b37e9c912dbb593c36393"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/ciutils/0.5.1/ciutils_linux_amd64.tar.gz"
-    sha256 "644bc44b4c816a6739c3a0f91e5fd8e1b6d43239b655df12854938a6445fa186"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/ciutils/0.6.1/ciutils_linux_amd64.tar.gz"
+    sha256 "2d6d00fe376222851ff46a3a146c9dd371b59fe450664e14533d9cb503a87c86"
   end
 
   def install
