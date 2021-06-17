510e1d743d3611f2971f5e1b04a074cb695ca98d
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-17 09:17:43 +0200

    Brew formula update for lcc-go-sdk version 0.0.4

--- Formula/lcc-cli.rb
+++ Formula/lcc-cli.rb
@@ -5,24 +5,24 @@
 class LccCli < Formula
   desc "Live Content API CLI Tools"
   homepage "https://navigaglobal.com"
-  version "0.0.3"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.3/lcc-go-sdk_macOS_64-bit.zip"
-    sha256 "15a65e542612711495cf8788460416ca672f98106929263bc613ddefb0b3d488"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.4/lcc-go-sdk_macOS_64-bit.zip"
+    sha256 "1ce5f308311f438bc1878d6692a57cbd4dda6af8314ba509e40d3db028cbd392"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.3/lcc-go-sdk_macOS_arm64.zip"
-    sha256 "35c029bc62a0dfa407821d787b6c98e00e4a784598f8d6f7b5d939fb5d677cfe"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.4/lcc-go-sdk_macOS_arm64.zip"
+    sha256 "9539484edd88c179948995c89355a91af2cb4e5bcb906d34406a5eccf15cc931"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.3/lcc-go-sdk_Tux_64-bit.zip"
-    sha256 "7ac34cc3680f669d65e7c7bc233fca0d9c2deea2e4e6e643b4b3e0831264fa7c"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.4/lcc-go-sdk_Tux_64-bit.zip"
+    sha256 "c42b60f219bfbc5df50daa381c95191643044906f56e49a8e28bbb7ef48008cb"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.3/lcc-go-sdk_Tux_arm64.zip"
-    sha256 "990500d94f5edee1bff5a21c9c3506da6719e6e2791771deacc8863d66a57d48"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.4/lcc-go-sdk_Tux_arm64.zip"
+    sha256 "7f9b87399dba431d7a65ff114010a21b4c9c27f6d26cfe1b4b900b0fc20fea96"
   end
 
   def install
