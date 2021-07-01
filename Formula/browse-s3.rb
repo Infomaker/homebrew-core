039772f2939bf78740ec9817c3d9f4873ea3e717
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-01 13:18:12 +0200

    Brew formula update for browse-s3 version v0.0.2

--- Formula/browse-s3.rb
+++ Formula/browse-s3.rb
@@ -5,24 +5,24 @@
 class BrowseS3 < Formula
   desc "CLI Tools"
   homepage "https://navigaglobal.com"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/browse-s3/0.0.1/browse-s3_macOS_64-bit.zip"
-    sha256 "9ea9161a2ec0ca9b328a7ea4893648619a206919399333525ae67020af3975a7"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/browse-s3/0.0.2/browse-s3_macOS_64-bit.zip"
+    sha256 "b718c42a180f18cd5f463ed7ded135d17f962fa58ec80d167fa28e9bc1f114cd"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/browse-s3/0.0.1/browse-s3_macOS_arm64.zip"
-    sha256 "a95404e8cd44e59293fb50ba2b52f93a2c10c625568d82a7a46dbbe1704231c9"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/browse-s3/0.0.2/browse-s3_macOS_arm64.zip"
+    sha256 "27792d4f7343a987b139b99a128d5b8ac07a37849326ef8edefb543fd4a5f4a6"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/browse-s3/0.0.1/browse-s3_Tux_64-bit.zip"
-    sha256 "e6eed817a0355b0792d9222f9196b086c78b4ba838bf3270ab2aede1224f75f0"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/browse-s3/0.0.2/browse-s3_Tux_64-bit.zip"
+    sha256 "e445bf30aa0a02072ef9a9f8df3c1df651c99d54bf33c6799455a7f1814cf0ff"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/browse-s3/0.0.1/browse-s3_Tux_arm64.zip"
-    sha256 "04f4936828dc69fddfbc2e2f1b5da2795ba382368f49a784912fa98bcfdf5cf3"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/browse-s3/0.0.2/browse-s3_Tux_arm64.zip"
+    sha256 "8873d0542c97ce2092613a011a2d95e14e420e699a15000e959ffaf137b92435"
   end
 
   def install
