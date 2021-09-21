287d0906b64ae5df69d589991033266331210b2f
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-21 10:29:13 +0200

    Brew formula update for imageengine-cli version v0.1.5

--- Formula/imageengine-cli.rb
+++ Formula/imageengine-cli.rb
@@ -5,16 +5,16 @@
 class ImageengineCli < Formula
   desc "OpenContent CLI Tools"
   homepage "https://navigaglobal.com"
-  version "0.1.4"
+  version "0.1.5"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/imageengine-cli/0.1.4/imageengine-cli_macOS_64-bit.zip"
-    sha256 "b5b0e0a4befe3b6e7b557c21562a8270ae43393799200db687481f33bba5008e"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/imageengine-cli/0.1.5/imageengine-cli_macOS_64-bit.zip"
+    sha256 "3978b63c3e6facb7ff06bdcbd8b55505d6a97f43c2b21c56843ccefd703d2ada"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/imageengine-cli/0.1.4/imageengine-cli_macOS_arm64.zip"
-    sha256 "585b84de390c5952b0fd630e736e68a62b9f0e137eb709941cb4b90333da344c"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/imageengine-cli/0.1.5/imageengine-cli_macOS_arm64.zip"
+    sha256 "89575fe9a02c6c47a79a60195ffa08e0647f5e71f6767db0f9cd613f2b7428c5"
   end
 
   def install
