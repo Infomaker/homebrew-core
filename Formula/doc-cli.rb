b769dd0b3ddd87ea26921c1be9fea6b162620a7c
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-21 11:25:55 +0200

    Brew formula update for doc-cli version v0.4.2

--- Formula/doc-cli.rb
+++ Formula/doc-cli.rb
@@ -5,24 +5,24 @@
 class DocCli < Formula
   desc "OpenContent CLI Tools"
   homepage "https://navigaglobal.com"
-  version "0.4.0"
+  version "0.4.2"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/doc-cli/0.4.0/doc-cli_macOS_64-bit.zip"
-    sha256 "7ae00ea501f78858999160fa32814d8136ef8a61a31ae22703a1ed1e281e18f6"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/doc-cli/0.4.2/doc-cli_macOS_64-bit.zip"
+    sha256 "901bf251b87b26ee55bfe3be45fe657c328da9c4c5fe340128b19793f6990fdd"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/doc-cli/0.4.0/doc-cli_macOS_arm64.zip"
-    sha256 "48217071bee16deff0bde0f2aded12837461ffd29ae6fccfa0aff1f313d1b5e2"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/doc-cli/0.4.2/doc-cli_macOS_arm64.zip"
+    sha256 "bf247a722a19375e01f680fbe2edb60c5408c6933c91f6ec26eb4d44da97ae1f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/doc-cli/0.4.0/doc-cli_Tux_64-bit.zip"
-    sha256 "bb1c4aaaafe7b261ddb7b51b4288866afb7967a3a1a37d90fd669382742979fe"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/doc-cli/0.4.2/doc-cli_Tux_64-bit.zip"
+    sha256 "4b440d8cfefa456f8c17f4007a4f4b9669abf8f485975245ed522e8be5b48022"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/doc-cli/0.4.0/doc-cli_Tux_arm64.zip"
-    sha256 "963acc216b30797fd3cfda725c040be055e74c4546eb1439d29525c2a3998dbd"
+    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/doc-cli/0.4.2/doc-cli_Tux_arm64.zip"
+    sha256 "bd781aa673e930e2444b124417d9ce04784251cda3d09600fdcd37be1df1b4a7"
   end
 
   def install
