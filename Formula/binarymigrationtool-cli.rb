4f65dc960c7a013af1b8be2f49f2ff0024bc11ae
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-24 19:54:45 +0200

    Brew formula update for binarymigrationtool version v0.1.0

--- Formula/binarymigrationtool-cli.rb
+++ Formula/binarymigrationtool-cli.rb
@@ -10,19 +10,19 @@ class BinarymigrationtoolCli < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/binarymigrationtool-cli/0.1.0/binarymigrationtool_macOS_64-bit.zip"
-    sha256 "b4b8254a441738a0ed39d3e4d60a5df874466bd988f181ceeb4e1bdfa39ab95f"
+    sha256 "8e35fa9d45ee7e584b8c1fc7a7ea01b7907c0d8423f71e03fb37c6529c70e8e0"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/binarymigrationtool-cli/0.1.0/binarymigrationtool_macOS_arm64.zip"
-    sha256 "3e69a4db9085be9cc0f58c76adfb0cd8f6e2c64e712ae8d2054f1a087420009a"
+    sha256 "10e6ae17c4d893acbe3df69bbe372056feb707405fbf1cbcffb90c64907bede0"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/binarymigrationtool-cli/0.1.0/binarymigrationtool_Tux_64-bit.zip"
-    sha256 "e823d574973c844f5a8bdd59fb752b6e985d11d4d00f1454cd906e5d5f970708"
+    sha256 "fb2e2947cd84bff14839b33a31069fc09eb9ebc3621004079d9be4e669fbe788"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/binarymigrationtool-cli/0.1.0/binarymigrationtool_Tux_arm64.zip"
-    sha256 "a378c9c17bfda856f45f45e2a993beb6f96160079d10f49650fcd2cff6030e17"
+    sha256 "2011337f943fe39fc895b7b157f2f1ded4f4c3f7dbbacce71fdf4b792c531e4e"
   end
 
   def install
