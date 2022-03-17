3b348bfa11fe1866230854f287417f174f385670
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-17 10:54:12 +0100

    Brew formula update for lcc-go-sdk version 0.0.7

--- Formula/lcc-cli.rb
+++ Formula/lcc-cli.rb
@@ -5,27 +5,43 @@
 class LccCli < Formula
   desc "Live Content API CLI Tools"
   homepage "https://navigaglobal.com"
-  version "0.0.6"
-  bottle :unneeded
+  version "0.0.7"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.6/lcc-go-sdk_macOS_64-bit.zip"
-    sha256 "27d24d399b94ec1aceaea08b5bc59769ddd4e705f3f24a052670669cfc691dab"
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.6/lcc-go-sdk_macOS_arm64.zip"
-    sha256 "6d5363a51aeead0a4005032c178ae0f7203991184e1d26f1fe0b48666666bcd9"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.6/lcc-go-sdk_Tux_64-bit.zip"
-    sha256 "ecbaa53d59d45e5caa338d579dd5f0b4ac140524b37724b6df4e7a745889341b"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.6/lcc-go-sdk_Tux_arm64.zip"
-    sha256 "03ec5365fd4de3b6b81319fe46e6ca50df399ec07b164b9f51476981824f4053"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.7/lcc-go-sdk_macOS_arm64.zip"
+      sha256 "a870813584e0595e1c159148df8661cb160b2cb49b156fa9a352e52007fc1ad9"
+
+      def install
+        bin.install "lcc-cli"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.7/lcc-go-sdk_macOS_64-bit.zip"
+      sha256 "3a033a4cf35c9718fe3bccdfedce435ecc19e6212e7eb96b0a8957054cec3fec"
+
+      def install
+        bin.install "lcc-cli"
+      end
+    end
   end
 
-  def install
-    bin.install "lcc-cli"
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.7/lcc-go-sdk_Tux_arm64.zip"
+      sha256 "e6380cfa3f7d963c111c9b7c640dbdf401b9fc3dc99db92cdba40c9e08921d0d"
+
+      def install
+        bin.install "lcc-cli"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/lcc-cli/0.0.7/lcc-go-sdk_Tux_64-bit.zip"
+      sha256 "e683f97907c0874a7274e48aeb5dd5d7eab9f7f6c39c3f4a1b2c75aed3efd55f"
+
+      def install
+        bin.install "lcc-cli"
+      end
+    end
   end
 end
