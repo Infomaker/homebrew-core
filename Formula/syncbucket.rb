6808707819d8fda289e2a954d0ad9af22099a1d6
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-23 20:11:49 +0100

    Brew formula update for syncbucket version v0.2.0

--- Formula/syncbucket.rb
+++ Formula/syncbucket.rb
@@ -5,27 +5,43 @@
 class Syncbucket < Formula
   desc "Utility for patching up image S3 buckets."
   homepage "https://bitbucket.org/infomaker/syncbucket/"
-  version "0.1.1"
+  version "0.2.0"
 
   on_macos do
+    if Hardware::CPU.arm?
+      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/syncbucket/0.2.0/syncbucket_darwin_arm64.zip"
+      sha256 "8a52bb63f48b837c6d60d6c3c749db20fbaf1b7f3abc162654f35477d786a0cc"
+
+      def install
+        bin.install "syncbucket"
+      end
+    end
     if Hardware::CPU.intel?
-      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/syncbucket/0.1.1/syncbucket_darwin_amd64.zip"
-      sha256 "760d082bf97b98707577797a0f8e219eed5a4c0b670781f13796840452becc25"
+      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/syncbucket/0.2.0/syncbucket_darwin_amd64.zip"
+      sha256 "2fbc0814b15ebe44465e73b08b5cf2947ada851da6918968c976300e8761721d"
+
+      def install
+        bin.install "syncbucket"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/syncbucket/0.1.1/syncbucket_linux_amd64.zip"
-      sha256 "871bbccd82548f14787057d6ac6a58ea0bdf53feaa8df6607c3fdc44e556e98c"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/syncbucket/0.1.1/syncbucket_linux_arm64.zip"
-      sha256 "f18a766ce692fe0df98d77d4e9a9fe822385548535709de9e044e099cb85a5ba"
-    end
-  end
+      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/syncbucket/0.2.0/syncbucket_linux_arm64.zip"
+      sha256 "736b175039251d7c61b1db7a59a4971dcea260742f7d0230eaba8303bbfa6182"
 
-  def install
-    bin.install "syncbucket"
+      def install
+        bin.install "syncbucket"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://im-saas-build-public-artifacts.s3-eu-west-1.amazonaws.com/tools/syncbucket/0.2.0/syncbucket_linux_amd64.zip"
+      sha256 "378e9728f7f7b9534fa25537abe9b3e63aaa0ba61e5c8d27bf1c34ff97c3accd"
+
+      def install
+        bin.install "syncbucket"
+      end
+    end
   end
 end
